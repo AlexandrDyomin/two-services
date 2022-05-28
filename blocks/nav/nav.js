7c59948d8c43ee79b8e1fdd2e6f5f2c0954578ea
Alexandr Dyomin <alexandr.dyomin@yahoo.com>
2022-05-28 20:24:29 +0300

    сверстал блок hero и карточки с услугами, сделал адаптив для хедера и hero

--- blocks/nav/nav.js
+++ blocks/nav/nav.js
@@ -4,7 +4,17 @@ if (burger) {
 	burger.addEventListener("click", handleBurgerClick);
 }
 
+
 function handleBurgerClick(e) {
 	let navList = this.nextElementSibling;
+
+	if (navList.classList.contains("nav__list_open")) {
+		setTimeout(() => {
+			navList.classList.add("nav__list_hidden");
+		}, 200);
+	} else {
+		navList.classList.remove("nav__list_hidden");
+	}
+
 	navList.classList.toggle("nav__list_open");
 }
