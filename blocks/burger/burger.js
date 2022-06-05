e31ea9d119b0ec1ac929ae3e251464c016bfe40f
Alexandr Dyomin <alexandr.dyomin@yahoo.com>
2022-06-05 18:56:38 +0300

    update

--- blocks/burger/burger.js
+++
@@ -1,9 +0,0 @@
-let burger = document.querySelector(".burger");
-
-if (burger) {
-	burger.addEventListener("click", handleBurgerClick)
-}
-
-function handleBurgerClick() {
-	this.classList.toggle("burger_active");
-}
